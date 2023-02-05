8706ee70eb0b0334e3a9f0ff879bea5703f98791
Elvis Silva <elvisfj@gmail.com>
2023-02-04 23:49:11 -0300

    Estudando

--- questao04.py
+++ questao04.py
@@ -14,7 +14,7 @@ menu_da_mercearia {
 
 def FUN_menu(menu: dict) -> None: #FUNÇÃO QUE IMPRIME AS OPÇÕES DOS MENUS
         for key, val in menu.itens():
-            print('{} - {val if type(val) == str else val[0]}'.format(key))
+            print(f"{key} - {val if type(val) == str else val[0]}")
         
         print('-' *35)
 
@@ -35,5 +35,45 @@ def mostrar_menu(menu: dict) -> int: #FUNÇÃO QUE IMPRIME E VALIDA A OPÇÃO ME
         except ValuError:
             print('\nOpção inválida!')
     return opcao
-def
+
+def cadastrarProduto(codigo: int) -> None: #FUNÇÃO QUE CADASTRA PRODUTOS
+    print('\nOpção Cadastrar Produtos\n')
+    print('Código do Produto {:>3}'.format(codigo))
+    
+    nome = input('Digite o nome do produto:').strip()
+    fabricante = input('Digite o fabricante do produto:').strip()
+    
+    while True:
+        try:
+            valor = float(input('Digite o valor (R$) do produto:'))
+            
+            #VERIFICA SE O VALOR É MAIOR
+            if valor <= 0:
+                print('\nALERTA!!! Digite um valor maior que zero!\n')
+            else:
+                break
+        except ValueError:
+            print('\nALERTA!!! Digite um número válido!\n')
+    
+    produtos[codigo] = []
+    produtos[].append(nome)
+    produtos[].append(fabricante)
+    produtos[].append(valor)
+
+def removerProduto() -> None: #FUNÇÃO QUE REMOVE PRODUTOS
+    while True:
+        try:
+            codigo = int(input('\nEscreva o código do produto a ser removido:\n'))
+            
+            #VERIFICA SE EXISTE O PRODUTO COM O CÓDIGO INFORMADO
+            # SE SIM, REMOVE O PRODUTO DO DICIONÁRIO
+            if codigo not in produto.keys():
+                print('\nNenhum produto possui esse código.')
+            else:
+                produtos.pop(codigo)
+            break
+        except ValueError:
+            print('\nALERTA!!! Digite um código válido!\n')
+            
+    
 
